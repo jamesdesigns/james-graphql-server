f8ee3f132d72cbb9b5d6cefe922508aa342ace0a
James Hooper <jameshooperdesigns@gmail.com>
2019-03-04 15:43:03 -0700

    Add extra details in the newUser

--- user-posts-example.js
+++ user-posts-example.js
@@ -6,11 +6,14 @@ async function main() {
   // Create a new user with a new post
   const newUser = await prisma
     .createUser({
-      name: "Bob",
-      email: "bob@prisma.io",
+      name: "Igor",
+      email: "igor@prisma.io",
       posts: {
         create: [{
           title: "Join us for GraphQL Conf in 2019",
+          comments: {
+            create: [{bodyText: 'I love GraphQl Conf!'},{bodyText: 'I love cookie dough!'}]
+          }
         }, {
           title: "Subscribe to GraphQL Weekly for GraphQL news",
         }]
@@ -19,11 +22,11 @@ async function main() {
   console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
 
   // Read all users from the database and print them to the console
-  const allUsers = await prisma.users()
-  console.log(allUsers)
+  // const allUsers = await prisma.users()
+  // console.log(allUsers)
 
-  const allPosts = await prisma.posts()
-  console.log(allPosts)
+  // const allPosts = await prisma.posts()
+  // console.log(allPosts)
 }
 
 main().catch(e => console.error(e))
