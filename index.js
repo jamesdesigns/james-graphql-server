7f7540248dd62366469e6a4a1a5d2cb6c26bc71f
James Hooper <jameshooperdesigns@gmail.com>
2019-04-01 12:47:49 -0600

    Edit comments

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ const { prisma } = require('./generated/prisma-client')
 // A `main` function so that we can use async/await
 async function main() {
 
-  // Create a New Product to add to the inventory
+  // CREATE a New Product to add to the inventory
+
   // const newProduct = await prisma.createProduct({
-  //   name: 'BigT', price: 25, color: 'Black', size: 'Large'
-  // })
+  //   name: 'BigT', price: 25, color: 'Black', size: 'Large' })
   // console.log(`Created new product: ${newProduct.name}, ${newProduct.size}, ${newProduct.price}, ${newProduct.color} (ID: ${newProduct.id})`)
 
-  // Read all existing products and print them out to the console
+  // READ all existing products and print them out to the console
   const allProducts = await prisma.products()
   console.log(allProducts)
 
-  // Update a Products name 
+  // UPDATE a Products name 
+
   // const updateProduct = await prisma.updateProduct({
   //   where: { id: 'cjtrjb7wm001l08394kzc0d4c'},
-  //   data: { name: 'BigHood' }
-  // })
+  //   data: { name: 'BigHood' }  })
 
-  // Delete a Product
+  // DELETE a Product
+
   // const deleteProduct = await prisma.deleteProduct({ id: 'PRODUCT_ID'})
 }
 
